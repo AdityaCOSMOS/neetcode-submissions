9592b9df5c84ee55f37ee2e64dce8370d341393e
Aditya Yadav <135484251+AdityaCOSMOS@users.noreply.github.com>
2026-05-31 02:06:36 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.length();
+        int nn = t.length();
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(n>nn){
+        for(int i = 0 ; i < n ; i++){
+            if(s[i]!=t[i]){
+                return false;
+            }
+        }
+        return true;
+        }
+        else{
+            for(int i = 0 ; i < nn ; i++){
+            if(s[i]!=t[i]){
+                return false;
+            }
+        }
+        return true;
+        }
+    }
+};
